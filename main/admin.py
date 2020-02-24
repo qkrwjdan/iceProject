79efad82a031b3d1094ceab30d5304e49ec67e0f
qkrwjdan <madogisa12@naver.com>
2020-02-25 00:36:42 +0900

    make Model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ElecAccountModel,NoticeModel
 # Register your models here.
+
+admin.site.register(ElecAccountModel)
+admin.site.register(NoticeModel)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import timedelta
+from django.utils import timezone
 
-# Create your models here.
+class ElecAccountModel(models.Model):
+    administrator = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default = timezone.now)
+    text = models.TextField()
+    amount = models.IntegerField(default=0)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.created_date + self.title
+
+class NoticeModel(models.Model):
+    administrator = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default = timezone.now)
+    text = models.TextField()
+    
+    def __str__(self):
+        return self.created_date + self.title
