535f1239336c73f255fb3fb8e611fd76dbc1410e
qkrwjdan <madogisa12@naver.com>
2020-02-24 16:30:56 +0900

    base.html 적용 (1)

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
      path("", views.index, name="index"),
+     path("/elecAccount",views.elecAccount,name="elecAccount"),
+     path("/introduce",views.introduce,name="introduce"),
+     path("/notice",views.notice,name="notice"),
+     
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'main/index.html',)
+
+def elecAccount(request):
+    return render(request,'main/elecAccount.html')
+
+def introduce(request):
+    return render(request,'main/introduce.html')
+
+def notice(request):
+    return render(request,'main/notice.html')
