35b3fce02829443eb45229fad74e397e91048a27
qkrwjdan <madogisa12@naver.com>
2020-02-25 21:23:12 +0900

    form lable modification

--- main/forms.py
+++ main/forms.py
@@ -4,11 +4,29 @@ from .models import ElecAccountModel,NoticeModel
 class ElecAccountForm(forms.ModelForm):
     class Meta:
         model = ElecAccountModel
-        fields = ['title','text','amount','image']
+        fields = ['title','administrator','text','amount','image']
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
+        #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        #     'password' : forms.PasswordInput(attrs={'class': 'form-control'}),
+        # }
+        labels = {
+            'title' : '제목',
+            'administrator': '작성자',
+            'text': '내용',
+            'amount' : '금액',
+            'image': '영수증 사진',
+        }
 
 class NoticeForm(forms.ModelForm):
     class Meta:
         model = NoticeModel
-        fields = ['title','text']
+        fields = ['title','administrator','text']
+
+        labels = {
+            'title' : '제목',
+            'administrator' : '작성자',
+            'text' : '내용',
+        }
 
         
