d1f1218f388811950ceb100cac6f28601e403a46
qkrwjdan <madogisa12@naver.com>
2020-02-25 15:45:39 +0900

    elecAccount, notice page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+from .forms import ElecAccountForm,NoticeForm
+from .models import ElecAccountModel,NoticeModel
 
 # Create your views here.
 
@@ -6,10 +11,20 @@ def index(request):
     return render(request,'main/index.html',)
 
 def elecAccount(request):
-    return render(request,'main/elecAccount.html')
+    elecData = ElecAccountModel.objects.all()
+    elecData = elecData.order_by("created_date")
+
+    return render(request,'main/elecAccount.html',{
+        "datas" : elecData,
+    })
+
+def notice(request):
+    noticeData = NoticeModel.objects.all()
+    noticeData = noticeData.order_by("created_date")
+
+    return render(request,'main/notice.html',{
+        "datas" : noticeData,
+    })
 
 def introduce(request):
     return render(request,'main/introduce.html')
-
-def notice(request):
-    return render(request,'main/notice.html')
